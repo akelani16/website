3d257a19c5de4288d19297099683a22db082d276
Adeniyi Kelani <akelani16@mba-loaner13-21.local>
2015-12-19 09:45:28 -0700

    Fixed HTML Errors

--- code.py
+++ code.py
@@ -2,11 +2,7 @@ import web
 render = web.template.render('templates/', base='layout')
 
 urls = (
-<<<<<<< HEAD
     '/', 'index', '/secondpage', 'secondpage', '/index', 'index', '/thirdpage', 'thirdpage'
-=======
-    '/', 'index', '/secondpage', 'secondpage', '/index', 'index'
->>>>>>> c8994afffaa8145f96de8ea8cc8683457a0e94fd
 )
 
 class index:
@@ -17,13 +13,10 @@ class secondpage:
     def GET(self):
         return render.secondpage()
 
-<<<<<<< HEAD
 class thirdpage:
     def GET(self):
         return render.thirdpage()
 
-=======
->>>>>>> c8994afffaa8145f96de8ea8cc8683457a0e94fd
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
